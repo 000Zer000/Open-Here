f4ae8fb127c46f581346e49c51a978471eeed956
TheOddZer0 <TheOddZer0@protonmail.com>
2021-03-15 11:17:57 +0330

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -2,7 +2,9 @@
 
 SET CWD=%~dp0
 
-ECHO Open-Here Installer, Installs all required registry files
+ECHO Open-Here Installer, Installs all required registry files, Written by TheOddZer0
+ECHO See COPYING for more info
+ECHO See the original repo for more details, updates: https://github.com/TheOddZer0/Open-Here/
 
 net session >nul 2>&1
 if %errorLevel% == 2 (
